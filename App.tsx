f6a0f2752df662207cbcd5627f60adf5435d12b3
Yuri <yuriqpaiva@gmail.com>
2022-09-28 19:09:25 -0300

    Add Sentry config

--- App.tsx
+++ App.tsx
@@ -3,11 +3,22 @@ import {StatusBar} from 'react-native';
 import {Home} from './src/pages/Home';
 import codePush from 'react-native-code-push';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://eee3cbc955a947b2aa9d926d024ec1b6@o1431087.ingest.sentry.io/6782022',
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+  environment: 'development',
+});
+
 function App() {
   useEffect(() => {
     codePush.sync({
       installMode: codePush.InstallMode.IMMEDIATE,
     });
+    throw new Error('Não foi possível abrir a aplicação!!!');
   }, []);
 
   return (
